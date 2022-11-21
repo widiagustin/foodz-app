ad38d20682bafbe9a2b13993d7c1b5a8b19f7610
widiagustin <widhianiagustin14@gmail.com>
2022-11-21 16:26:13 +0700

    refactor: Change Button component to using button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,10 @@ import Link from "next/link";
 export default function Button (props) {
   return (
     <div>
-      <Link href={props.link}
-        className='py-2 px-5 rounded-lg bg-primary text-white hover:bg-hover-primary'>
-        {props.title}
+      <Link href={props.link}>
+        <button className='py-2 px-5 rounded-lg bg-primary text-white hover:bg-hover-primary'>
+          {props.title}
+        </button>
       </Link>
     </div>
   )
