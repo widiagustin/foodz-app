e2b85ccf2a27be628d4a75bd15a011314c1f9e95
widiagustin <widhianiagustin14@gmail.com>
2022-11-21 16:25:03 +0700

    feat: add flowbite-react configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     './src/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
