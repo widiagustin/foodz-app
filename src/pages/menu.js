e4286c2bd0cdd1fae4c44188bf473ae1babee178
widiagustin <widhianiagustin14@gmail.com>
2022-11-23 20:28:48 +0700

    feat: Add Data fetching, Pagination & Skeleton

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,13 +1,50 @@
-import { BsTags } from 'react-icons/bs'
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import Card from '../components/Card';
-import tokpokki from '../images/Tokpokki.png'
 import Tag from '../components/Tag';
 import { Pagination } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Skeleton from '../components/Skeleton';
 
 
 export default function Menu () {
+  const [product, setProduct] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [page, setPage] = useState(1)
+  const [pages, setPages] = useState(0)
+  const [limit, setLimit] = useState(6)
+
+  const BaseURL = `https://food-backend-production.up.railway.app/api/products/?page=${page}&limit=${limit}`
+
+  useEffect(async () => {
+    setIsLoading(true)
+
+    new Promise(res => {
+      setTimeout(() => {
+        res()
+      }, 500)
+    }).then(() => {
+      axios.get(BaseURL).then(res => {
+        setProduct(res.data.data)
+        setIsLoading(false)
+        setPage(res.data.currentPage)
+        setPages(res.data.totalPages)
+      })
+    })
+  }, [page, limit])
+
+  const getProducts = async () => {
+    const res = await axios.get(BaseURL)
+    setProduct(res.data.data)
+    setPage(res.data.currentPage)
+    setPages(res.data.totalPages)
+  }
+
+  const changePage = (data) => {
+    setPage(data)
+  }
+
   return (
     <div>
       <Navbar />
@@ -29,55 +66,30 @@ export default function Menu () {
         </div>
         <div className='max-w-6xl text-center grid'>
           <div className='grid-cols-3 gap-16 grid place-content-center mx-auto'>
-            <Card
-              src={tokpokki}
-              title='Korea BBQ'
-              desc='lorem ipsum dolor sit amet yuhuy asbda'
-              price='Rp. 20.000'
-              btnLink=''>
-            </Card><Card
-              src={tokpokki}
-              title='Korea BBQ'
-              desc='lorem ipsum dolor sit amet yuhuy asbda'
-              price='Rp. 20.000'
-              btnLink=''>
-            </Card>
-            <Card
-              src={tokpokki}
-              title='Korea BBQ'
-              desc='lorem ipsum dolor sit amet yuhuy asbda'
-              price='Rp. 20.000'
-              btnLink=''>
-            </Card>
-            <Card
-              src={tokpokki}
-              title='Korea BBQ'
-              desc='lorem ipsum dolor sit amet yuhuy asbda'
-              price='Rp. 20.000'
-              btnLink=''>
-            </Card>
-            <Card
-              src={tokpokki}
-              title='Korea BBQ'
-              desc='lorem ipsum dolor sit amet yuhuy asbda'
-              price='Rp. 20.000'
-              btnLink=''>
-            </Card>
-            <Card
-              src={tokpokki}
-              title='Korea BBQ'
-              desc='lorem ipsum dolor sit amet yuhuy asbda'
-              price='Rp. 20.000'
-              btnLink=''>
-            </Card>
+            {isLoading ? new Array(6).fill(1).map((_, i) => {
+              return <Skeleton key={i} />;
+            }) :
+              product.map(item => (
+                <div key={item._id}>
+                  <Card
+                    src={item.image_url}
+                    title={item.name}
+                    desc={item.description}
+                    price={item.price}
+                    btnLink=''>
+                  </Card>
+                </div>
+              ))
+            }
           </div>
         </div>
         <div className='flex justify-center mt-14'>
           <Pagination
             currentPage={1}
-            totalPages={100}
-            onPageChange={''}
-            className={'paginate'}
+            totalPages={pages}
+            onPageChange={changePage}
+            showIcons={true}
+            layout={'pagination'}
           />
         </div>
       </div>
