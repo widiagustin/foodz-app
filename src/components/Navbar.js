6a03d71131e46d9a0f7ca9362c88ab949fb1383d
widiagustin <widhianiagustin14@gmail.com>
2022-11-25 16:38:30 +0700

    feat: Set router styling for active path navbarg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,12 @@ import Link from 'next/link'
 import Button from '../components/Button'
 import { Navbar } from 'flowbite-react'
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { BsCart, BsPerson } from 'react-icons/bs'
+import { IconContext } from 'react-icons'
 
 export default function () {
+  const router = useRouter()
   const [navbar, setNavbar] = useState(false)
 
   const changeBackground = () => {
@@ -13,6 +17,14 @@ export default function () {
       setNavbar(true)
     } else {
       setNavbar(false)
+    }
+  }
+
+  const isActive = (r) => {
+    if (r === router.pathname) {
+      return "active"
+    } else {
+      return ""
     }
   }
 
@@ -29,7 +41,7 @@ export default function () {
         : 'py-6 w-full backdrop-filter backdrop-blur-lg bg-opacity-90 z-50 fixed top-0'}
     >
       <div className='flex w-[72rem] mx-auto justify-between'>
-        <Navbar.Brand href="https://flowbite.com/">
+        <Navbar.Brand href="/">
           <Image
             src={Logo} width={65} alt='Foodz Logo'
           />
@@ -37,16 +49,43 @@ export default function () {
         <Navbar.Toggle />
         <ul className='flex items-center gap-10 text-sm'>
           <li className='hover:text-hover-primary'>
-            <Link href="/">Beranda</Link>
+            <Link href="/">
+              <a className={isActive('/')}>
+                Beranda
+              </a>
+            </Link>
           </li>
           <li className='hover:text-hover-primary'>
-            <Link href="/menu">Menu</Link>
+            <Link href="/menu">
+              <a className={isActive('/menu')}>
+                Menu
+              </a>
+            </Link>
           </li>
           <li className='hover:text-hover-primary'>
-            <Link href="/cart">Keranjang</Link>
+            <Link href="/cart">
+              <a className={isActive('/cart')}>
+                <div className='flex items-center gap-2'>
+                  <div className='bg-red-600 h-4 w-4 rounded-full relative left-8 top-2'>
+                    <p className='text-xs text-center text-white'>0</p>
+                  </div>
+                  <IconContext.Provider
+                    value={{ size: '15px' }}>
+                    <BsCart />
+                  </IconContext.Provider>
+                </div>
+              </a>
+            </Link>
           </li>
           <li className='hover:text-hover-primary'>
-            <Link href="/profile">Profile</Link>
+            <Link href="/profile">
+              <a className={isActive('/profile')}>
+                <IconContext.Provider
+                  value={{ size: '18px' }}>
+                  <BsPerson />
+                </IconContext.Provider>
+              </a>
+            </Link>
           </li>
           <li>
             <Button title='Login' link='/login'></Button>
