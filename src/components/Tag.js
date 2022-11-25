26167b341a6481303d1bd19c4b72dca5a14c92f5
widiagustin <widhianiagustin14@gmail.com>
2022-11-25 16:39:42 +0700

    refactor : Change Link to props onClick in Tag component

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,13 +1,14 @@
-import Link from 'next/link'
 import { BsTag } from 'react-icons/bs'
 
 export default function Tag (props) {
   return (
-    <Link href={props.link}>
-      <div className='border border-primary text-primary rounded-full items-center gap-3 flex py-2 px-3 hover:bg-primary hover:text-white'>
+    <div>
+      <div
+        onClick={props.onClick}
+        className='bg-neutral-7 px-2 py-1 gap-1 text-sm text-neutral-3 rounded-full flex items-center hover:text-primary hover:bg-light-primary hover:cursor-pointer'>
         <BsTag />
-        <p>{props.title}</p>
+        <p>{props.name}</p>
       </div>
-    </Link>
+    </div>
   )
 }
