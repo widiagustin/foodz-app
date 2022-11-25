122785c9a07465d7914029795f7813a24c297d60
widiagustin <widhianiagustin14@gmail.com>
2022-11-25 16:36:15 +0700

    refactor: Change styling and add icon in Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Button from './Button'
-import { BsCart } from 'react-icons/bs'
+import { BsCart, BsTag } from 'react-icons/bs'
 import Link from 'next/link'
 
 export default function Card (props) {
@@ -9,8 +9,13 @@ export default function Card (props) {
       <div className='border border-neutral-100 w-72 rounded-2xl p-5 shadow-sm bg-white hover:shadow-md'>
         <Image src={props.src} width={400} height={250} className='mx-auto object-cover rounded-lg' alt='Product Item'></Image>
         <h1 className='font-alice text-left text-neutral-2 text-2xl mt-3'>{props.title}</h1>
-        <p className='text-sm text-neutral-4 text-left my-1'>{props.desc}</p>
-        <div className='flex justify-between mt-6 items-center'>
+        <div className='flex mt-2'>
+          <p className='text-xs px-2 py-1 text-neutral-4 bg-neutral-7 rounded-full flex gap-1 items-center'>
+            <BsTag />
+            {props.tag}
+          </p>
+        </div>
+        <div className='flex justify-between mt-4 items-center'>
           <p className='text-primary font-alice text-xl font-semibold'>Rp. {props.price}</p>
           <Link href={props.btnLink}>
             <button
