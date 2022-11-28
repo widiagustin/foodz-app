67b5e027f6e9d2885d69346abae83328f10d2339
widhianiagustin <widhianiagustin14@gmail.com>
2022-11-28 08:42:16 +0700

    feat: Add onClick in Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,9 @@ export default function Button (props) {
   return (
     <div>
       <Link href={props.link}>
-        <button className='py-2 px-5 rounded-lg bg-primary text-white hover:bg-hover-primary'>
+        <button
+          onClick={props.onClick}
+          className='py-2 px-5 rounded-lg bg-primary text-white hover:bg-hover-primary'>
           {props.title}
         </button>
       </Link>
