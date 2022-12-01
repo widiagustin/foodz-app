692bdf68727e8f639660eb0c4bde2d593d5c65cd
widhianiagustin <widhianiagustin14@gmail.com>
2022-12-01 14:19:13 +0700

    fix: Fixed error on props Button Link

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 
 export default function Button (props) {
+  const href = props.link ? { href: props.link } : { href: '' }
+
   return (
     <div>
-      <Link href={props.link}>
+      <Link {...href}>
         <button
           onClick={props.onClick}
           className='py-2 px-5 rounded-lg bg-primary text-white hover:bg-hover-primary'>
